3d9227df8d18710244a58d0ae7ffb0985930a6d8
Kafiynik <hiikitoss@gmail.com>
2023-03-30 17:42:06 +0300

    тесчу получение ссылки на стили из hmtl'ки

--- static/js/navi.js
+++ static/js/navi.js
@@ -1,12 +1,17 @@
 let nav_theme = document.getElementById("switch_nav")
 let day_night_text = document.getElementById("go_day_night")
 
+let dayLink = document.getElementById("day_href_css")
+let nightLink = document.getElementById("night_href_css")
+
 if (localStorage.getItem('theme') == "true"){
-    nav_theme.href = '../dark-theme/navigation-dark.css'
+    //nav_theme.href = '../dark-theme/navigation-dark.css'
+    nav_theme.href = nightLink.href
     if (day_night_text){day_night_text.innerHTML = 'На дневную'}
 }
 
 else{
-    nav_theme.href = '../css/base.css'
+    //nav_theme.href = '../dark-theme/navigation-dark.css'
+    nav_theme.href = dayLink.href
     if (day_night_text){day_night_text.innerHTML = 'На ночную'}
 }
